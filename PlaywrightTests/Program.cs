058331e6d9dbecde996d55504d3e3d575f0253e8
kushal <kushaldeveloper@gmail.com>
2021-09-26 08:32:43 -0600

    add a playwright tests console applicaltion?

---
+++ PlaywrightTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlaywrightTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
