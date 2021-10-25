b556510c49283a16975d89d313a7441bb0f24a60
kushal <kushaldeveloper@gmail.com>
2021-10-25 07:20:24 -0600

    add rider

--- PlaywrightTests/Program.cs
+++ PlaywrightTests/Program.cs
@@ -2,9 +2,9 @@
 
 namespace PlaywrightTests
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
